0576083b2decc1373981f998cddb0a41d491a5e1
brigeBook <k9mm10feet@yahoo.co.jp>
2025-04-30 16:24:15 +0900

    アカウンんと登録一時コミット

--- backend/src/routes/apiRoutes.ts
+++ backend/src/routes/apiRoutes.ts
@@ -1,39 +1,51 @@
-import express, { Request, Response } from 'express'
-import Post from '../models/post'
+import express, { Request, Response } from "express";
+import Post from "../models/post";
 
-const router = express.Router()
+const router = express.Router();
 
 // 一覧取得
-router.get('/getList', async (req: Request, res: Response) => {
-    try {
-        const posts = await Post.find().sort({ createdAt: -1 })
-        res.json(posts)
-    } catch (err) {
-        res.status(500).json({ error: 'サーバーエラー' })
-    }
-})
+router.get("/getList", async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: "サーバーエラー" });
+  }
+});
 
 // 詳細取得
-router.get('/getDetail', async (req: Request, res: Response) => {
-    const id = req.query.id;
-    try {
-        const posts = await Post.findById(id);
-        res.json(posts)
-    } catch (err) {
-        res.status(500).json({ error: 'サーバーエラー' })
-    }
-})
+router.get("/getDetail", async (req: Request, res: Response) => {
+  const id = req.query.id;
+  try {
+    const posts = await Post.findById(id);
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: "サーバーエラー" });
+  }
+});
 
 // 新規投稿
-router.post('/posts', async (req: Request, res: Response) => {
-    try {
-        const { title, content } = req.body
-        const newPost = new Post({ title, content })
-        const savedPost = await newPost.save()
-        res.status(201).json(savedPost)
-    } catch (err) {
-        res.status(400).json({ error: '投稿に失敗しました' })
-    }
-})
+router.post("/posts", async (req: Request, res: Response) => {
+  try {
+    const { title, content } = req.body;
+    const newPost = new Post({ title, content });
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ error: "投稿に失敗しました" });
+  }
+});
 
-export default router
+// アカウント登録
+router.post("/posts", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const newPost = new Post({ email, password });
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ error: "投稿に失敗しました" });
+  }
+});
+
+export default router;
